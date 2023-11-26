b4e2a238e04d80fe551a96023026709037745792
dracorexgw <102985085+dracorexgw@users.noreply.github.com>
2023-11-25 23:20:28 -0600

    Changed Provider to parent and app to child

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,15 @@
+import './initWindowGlobals.js';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx';
+import { ChatbotContainerProvider } from '@/context/chatbox.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChatbotContainerProvider>
+      <App />
+    </ChatbotContainerProvider>
   </React.StrictMode>,
 )
+
+
