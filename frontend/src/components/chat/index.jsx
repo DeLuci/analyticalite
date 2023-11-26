34152595ed5054d674d6450f842eb912bdc292c4
dracorexgw <102985085+dracorexgw@users.noreply.github.com>
2023-11-25 23:26:41 -0600

    Modal for file upload

--- frontend/src/components/chat/index.jsx
+++ frontend/src/components/chat/index.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useContext } from 'react';
 import './index.css';
 import axios from 'axios'
-import {TableContext} from "@/context/db.jsx";
+import { TableContext } from "@/context/db.jsx";
+// Import all of Bootstrap's JS
+
 const Chatbot = () => {
     const [messages, setMessages] = useState([]);
     const [input, setInput] = useState('');
     const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState('');
+
+    // Not used yet.
     const [databaseInfo, setDatabaseInfo] = useState({})
-    const {tableResponse} = useContext(TableContext)
+    const { tableResponse } = useContext(TableContext)
+
     const handleFileChange = (e) => {
         const selectedFile = e.target.files[0];
         if (selectedFile) {
@@ -23,13 +29,30 @@ const Chatbot = () => {
 
     };
 
-    const uploadFile = async (e) => {
-        console.log("here")
-        e.preventDefault()
-        if (!file) return;
+    const modalAlert = () => {
+        let myModal = new bootstrap.Modal(document.getElementById('add-filename'), {
+            keyboard: false
+        });
+        myModal.show();
+    };
 
+    const validateFileName = async () => {
+        if(fileName)
+            await uploadFile();
+    }
+
+    const printFormData = (formData) => {
+       // Get the values of the FormData object
+       formData.forEach((value, key) => {
+            console.log(`${key}: ${value}`);
+        });
+    }
+
+    const uploadFile = async () => {
+        if (!file) return;
         const formData = new FormData();
         formData.append('file', file)
+        formData.append('fileName', fileName);
 
         try {
             const response = await axios.post('http://localhost:8000/load', formData, {
@@ -38,15 +61,24 @@ const Chatbot = () => {
                 }
             });
             console.log(response)
-            setMessages(msgs => [...msgs, {text: "File uploaded successfully", sender: "bot"}]);
+            setMessages(msgs => [...msgs, { text: "File uploaded successfully", sender: "bot" }]);
         } catch (error) {
             console.error('Error uploading file:', error);
         }
 
         setFile(null);
     }
-    const sendMessage = (e) => {
+
+    const submitForm = async (e) => {
         e.preventDefault();
+        if(file){
+            modalAlert();
+            await validateFileName();
+        }
+        sendMessage();
+    };
+
+    const sendMessage = () => {
         if (!input.trim()) return;
         console.log("passes here")
         setMessages([...messages, { text: input, sender: 'user' }]);
@@ -58,36 +90,56 @@ const Chatbot = () => {
     };
 
     return (
-        <div className="chat-box">
-            <div className="chat-window">
-                <div className="chat-top">
-                    <p>Label: {tableResponse?.label}</p>
-                    <p>Attribute: {tableResponse?.attribute}</p>
-                </div>
-                <div className="chat-bottom">
-                    {messages.map((message, index) => (
-                    <div key={index} className={`chat-message ${message.sender}-message`}>
-                        <p>{message.text}</p>
+        <>
+            <div className="chat-box">
+                <div className="chat-window">
+                    <div className="chat-top">
+                        {messages.map((message, index) => (
+                            <div key={index} className={`chat-message ${message.sender}-message`}>
+                                <p>{message.text}</p>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
+                <form onSubmit={(e) => submitForm(e)}>
+                    <div className="custom-upload-container">
+                        <label htmlFor="file-upload" className="custom-file-upload">
+                            <i className="bi bi-paperclip"></i>
+                        </label>
+                        <input id="file-upload" type="file" onChange={handleFileChange} />
+                    </div>
+                    <input
+                        type="text"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        className="chat-input"
+                    />
+                    <button type="submit" className="send-button"><i className="bi bi-send"></i></button>
+                </form>
+            </div>
+            <div className="modal fade" id="add-filename" tabIndex="-1" aria-labelledby="exampleModalCenteredScrollableTitle" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalCenteredScrollableTitle">Uploaded Filename</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="form-floating mb-3">
+                                    <input type="email" className="form-control" id="floatingInput" name="add_filename" value={fileName} onChange={(e) => setFileName(e.target.value)} placeholder="example_db" />
+                                    <label htmlFor="floatingInput">Filename</label>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-dark" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-success" onClick={() => validateFileName()}>Save changes</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <form onSubmit={sendMessage}>
-                <div className="custom-upload-container">
-                    <label htmlFor="file-upload" className="custom-file-upload">
-                        <i className="bi bi-file-arrow-up"></i>
-                    </label>
-                    <input id="file-upload" type="file" onChange={handleFileChange}/>
-                </div>
-                <input
-                    type="text"
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    className="chat-input"
-                />
-                <button type="submit" className="send-button"><i className="bi bi-send"></i></button>
-            </form>
-        </div>
+        </>
     );
 };
 
