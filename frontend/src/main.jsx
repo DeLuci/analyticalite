f96cf84dc92fd82b009a1f8823e53a2b79a5085b
Larroyo <95649169+DeLuci@users.noreply.github.com>
2023-11-30 17:11:59 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import { ChatbotContainerProvider } from '@/context/chatbox.jsx';
 import { MenuContainerProvider } from './context/menu.jsx';
+import { DatabaseUpdateProvider } from '@/context/databaseUpdateContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MenuContainerProvider>
-      <ChatbotContainerProvider>
-        <App />
-      </ChatbotContainerProvider>
-    </MenuContainerProvider>
+      <DatabaseUpdateProvider>
+          <MenuContainerProvider>
+              <ChatbotContainerProvider>
+                  <App/>
+              </ChatbotContainerProvider>
+          </MenuContainerProvider>
+      </DatabaseUpdateProvider>
   </React.StrictMode>,
 )
 
